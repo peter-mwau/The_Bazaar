9a13ab9cefb64717e227f7dcaf9d16bb44ec25e6
peter-mwau <kyalepeter2000@gmail.com>
2026-03-28 15:38:36 +0300

    successfully tested the token contract

--- test/tokenTest.js
+++ test/tokenTest.js
@@ -20,14 +20,76 @@ beforeEach(async function () {
 });
 
 
-describe("Token", function () {
+describe("Deployment", function () {
     it("It should ensure that contract is deployed", async function () {
         const tokenAddress = await this.token.getAddress();
         expect(tokenAddress).to.match(/^0x[a-fA-F0-9]{40}$/);
     });
+});
+
+describe("Token Metadata", function () {
 
     it("Should ensure fungible token metadata is correct", async function () {
         expect(await this.token.name()).to.equal("MyToken");
         expect(await this.token.symbol()).to.equal("MTK");
+    });
+});
+
+describe("Minting", function () {
+    it("Should allow owner to mint tokens", async function () {
+        const mintAmount = ethers.parseEther("1000");
+        const initialSupply = await this.token.INITIAL_SUPPLY();
+
+        await this.token.mintToken(owner.address, mintAmount);
+        const ownerBalance = await this.token.balanceOf(owner.address);
+        expect(ownerBalance).to.equal(initialSupply + mintAmount);
+    });
+
+    it("Should prevent non-minters from minting tokens", async function () {
+        const mintAmount = ethers.parseEther("1000");
+        await expect(this.token.connect(_addr1).mintToken(_addr1.address, mintAmount)).to.be.reverted;
+    });
+});
+
+describe("Total Supply", function () {
+    it("Should return the correct total supply after minting", async function () {
+        const mintAmount = ethers.parseEther("1000");
+        const initialSupply = await this.token.INITIAL_SUPPLY();
+
+        await this.token.mintToken(owner.address, mintAmount);
+        const totalSupply = await this.token.totalSupply();
+        expect(totalSupply).to.equal(initialSupply + mintAmount);
+    });
+});
+
+describe("Max Supply", function () {
+    it("Should prevent minting beyond max supply", async function () {
+        const maxSupply = await this.token.MAX_SUPPLY();
+        const initialSupply = await this.token.INITIAL_SUPPLY();
+        const remainingMintable = maxSupply - initialSupply;
+
+        await this.token.mintToken(owner.address, remainingMintable);
+        await expect(this.token.mintToken(owner.address, 1)).to.be.revertedWith(
+            "MAX SUPPLY EXCEEDED"
+        );
+    });
+});
+
+describe("Burning", function () {
+    it("Should allow owner to burn tokens from an address", async function () {
+        const mintAmount = ethers.parseEther("1000");
+        const initialSupply = await this.token.INITIAL_SUPPLY();
+
+        await this.token.burnToken(owner.address, mintAmount);
+        const ownerBalance = await this.token.balanceOf(owner.address);
+        expect(ownerBalance).to.equal(initialSupply - mintAmount);
+    });
+
+    it("Should prevent burning more tokens than balance", async function () {
+        const initialSupply = await this.token.INITIAL_SUPPLY();
+
+        await expect(this.token.burnToken(owner.address, initialSupply + 1n)).to.be.revertedWith(
+            "INSUFFICIENT BALANCE"
+        );
     });
 });
