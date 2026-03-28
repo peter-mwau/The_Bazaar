b624b103775ccffdd3ad8bdabb76b47f6bf9590f
peter-mwau <kyalepeter2000@gmail.com>
2026-03-28 10:17:36 +0300

    set a few configurations and installed dependancies

--- hardhat.config.cjs
+++ hardhat.config.cjs
@@ -2,5 +2,15 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.28",
+  solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.VITE_SEPOLIA_RPC_URL || "",
+      accounts:
+        process.env.VITE_PRIVATE_KEY !== undefined ? [process.env.VITE_PRIVATE_KEY] : [],
+    },
+  },
+  etherscan: {
+    apiKey: process.env.VITE_ETHERSCAN_API_KEY,
+  },
 };
