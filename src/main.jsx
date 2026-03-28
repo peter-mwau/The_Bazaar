027514f7a3d9b98e1692d93101908a68389f54d5
peter-mwau <kyalepeter2000@gmail.com>
2026-03-28 21:34:04 +0300

    configured the thirdweb, pinata & added some pages

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
+import { ThirdwebProvider } from "thirdweb/react";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <ThirdwebProvider>
+        <App />
+      </ThirdwebProvider>
     </BrowserRouter>
   </StrictMode>,
 );
